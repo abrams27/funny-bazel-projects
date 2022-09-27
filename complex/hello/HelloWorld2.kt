5063b1539fbe83f033638ef97f5943b70b47627b
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2022-09-27 17:51:37 +0200

    complex project

---
+++ complex/hello/HelloWorld2.kt
@@ -0,0 +1,10 @@
+import lib.Lib.a
+
+object HelloWorld2 {
+  
+  @JvmStatic
+  fun main(args: Array<String>) {
+    println("Hello $a!")
+  }
+}
+
---
+++ complex/hello/lib/Lib.kt
@@ -0,0 +1,6 @@
+package lib
+
+object Lib {
+  val a = "bazel"
+}
+
